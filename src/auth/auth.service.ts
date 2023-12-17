a828439d5384590cf25107bd7181c2f2a62cf8fd
alikalatearabi <ali.kalate89@hotmail.com>
2023-12-17 23:51:07 +0330

    initial auth controller and database

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from "@nestjs/common";
+
+
+@Injectable({})
+export class AuthService {
+    signup(){
+        return {msg: "sign up"}
+    }
+
+    signin(){
+        return {msg: "sign in"}
+    }
+}
